20da62bdb4f36a3ed536c1485500f133c69a582f
isferdinand <ikipkogey@kabarak.ac.ke>
2024-11-22 18:31:08 +0300

    Implement chat functionality

--- app/chat/index.jsx
+++ app/chat/index.jsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { addDoc, collection, doc, getDoc, onSnapshot } from 'firebase/firestore'
+import React, { useEffect, useState} from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import { db } from '../../config/FirebaseConfig'
+import moment from 'moment'
 
 export default function ChatScreen() {
+  const [messages, setMessages] = useState([])
+  const params = useLocalSearchParams() 
+  const navigation = useNavigation()
+  const {user} = useUser()
+
+  useEffect(() => {
+    GetUserDetails()
+    const unsubscribe = onSnapshot(collection(db, 'Chat', params?.id, 'Messages'),
+     (snapshot) =>{
+        const messageData = snapshot.docs.map((doc) => ({
+          _id:doc.id,
+          ...doc.data()
+        }))
+        setMessages(messageData)
+     })
+     return () => unsubscribe()
+  }, [])
+
+
+  const GetUserDetails = async() => {
+    const docRef = doc(db, 'Chat', params?.id);
+    const docSnap = await getDoc(docRef);
+
+    const result = docSnap.data()
+    // console.log(result)
+
+    //remember to change the == to !==
+    const otherUser = result?.users?.filter(item => item.email == user?.primaryEmailAddress?.emailAddress)
+    // console.log(otherUser)
+    navigation.setOptions({
+      headerTitle: otherUser[0]?.name 
+    })  
+  }
+
+  const onSend = async (newMessage) => {
+    setMessages(previousMessage => GiftedChat.append(previousMessage, newMessage))
+    newMessage[0].createdAt = moment().format('MM-DD-YYYY HH:mm:ss')
+    await addDoc(collection(db, 'Chat', params?.id,'Messages'), newMessage[0] )
+
+  }
+
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      showUserAvatar={true}
+      user={{
+        _id: user?.primaryEmailAddress?.emailAddress,
+        name: user?.fullName,
+        avatar: user?.imageUrl
+      }}
+    />
   )
 }
