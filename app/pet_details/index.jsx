6bca1f779a5f947924323b28a866ae308febff18
isferdinand <ikipkogey@kabarak.ac.ke>
2024-11-22 18:31:58 +0300

    Add chat initiation functionality

--- app/pet_details/index.jsx
+++ app/pet_details/index.jsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams, useNavigation } from 'expo-router';
+import { useLocalSearchParams, useNavigation, useRouter } from 'expo-router';
 import React, { useEffect } from 'react';
 import { View, Text, ScrollView, TouchableOpacity, StyleSheet  } from 'react-native';
 import PetInfo from '../../components/PetDetails/PetInfo';
@@ -6,11 +6,16 @@ import PetSubInfo from '../../components/PetDetails/PetSubInfo';
 import AboutPet from '../../components/PetDetails/AboutPet';
 import OwnerInfo from '../../components/PetDetails/OwnerInfo';
 import Colors from '../../constants/Colors'
+import { useUser } from '@clerk/clerk-expo';
+import { collection, getDocs, query, setDoc, where, doc } from 'firebase/firestore';
+import { db } from '../../config/FirebaseConfig';
 
 export default function PetDetails() {
 
   const pet = useLocalSearchParams();
   const navigation = useNavigation();
+  const {user} = useUser()
+  const router = useRouter()
 
   useEffect(() => {  
     // console.log(pet)
@@ -19,6 +24,49 @@ export default function PetDetails() {
         headerTitle: '',
     })
   }, [])
+
+
+  /*
+  Used to initiate a chat between the user and the pet owner
+  */
+  const InitiateChat = async () => {
+      const docId1 = user?.primaryEmailAddress?.emailAddress+ '_'+ pet?.email;
+      const docId2 = pet?.email+ '_'+ user?.primaryEmailAddress?.emailAddress;
+
+      const q = query(collection(db, 'Chat'), where('id', 'in', [docId1, docId2]));
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach(doc => {
+        console.log(doc.data())
+        router.push({
+          pathname: '/chat',
+          params: {id:doc.id}
+      })
+      })
+
+      if(querySnapshot.docs?.length==0)
+      {
+        await setDoc(doc(db, 'Chat', docId1), {
+          id:docId1,
+          users: [
+            {
+              email: user?.primaryEmailAddress?.emailAddress,
+              imageUrl: user?.imageUrl,
+              name: user?.fullName
+            },
+            {
+              email: pet?.email,
+              imageUrl: pet?.userImage,
+              name: pet?.username
+            }
+          ],
+          userIds:[user?.primaryEmailAddress?.emailAddress, pet?.email]
+        })
+        router.push({ 
+          pathname: '/chat',
+          params: {id:docId1}
+        })
+      }
+  }
  
   return (
     <View>
@@ -37,7 +85,10 @@ export default function PetDetails() {
       </ScrollView>
       {/* Adopt me buttton */}
       <View style={styles?.bottomContainer}>
-            <TouchableOpacity style={styles.adoptButton}>
+            <TouchableOpacity 
+              style={styles.adoptButton}
+              onPress={InitiateChat}
+            >
               <Text
                 style={{
                   fontFamily:'poppins-medium',
